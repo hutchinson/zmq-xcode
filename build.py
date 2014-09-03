2aebcb9fe869490e2fd0fa5360f6dd66a6379bca
Dan Hutchinson <d.hutchinson4@googlemail.com>
2014-09-03 20:50:08 +0100

    Add c++ bindings and add minor optimisation for not rebuilding when it's already built

--- build.py
+++ build.py
@@ -15,15 +15,26 @@ if len(sys.argv) > 1 and sys.argv[1] == "clean":
   shutil.rmtree(BUILD_DIR, ignore_errors=True)
   exit(0)
 
+# Poor man's dependency tracking...
+static_lib = os.path.join(BUILD_DIR, "lib", "libzmq.a")
+header_c_zmq = os.path.join(BUILD_DIR, "include", "zmq.h")
+header_cpp_zmq = os.path.join(BUILD_DIR, "include", "zmq.hpp")
+
+if os.path.exists(static_lib) and os.path.exists(header_c_zmq) and os.path.exists(header_cpp_zmq):
+  exit(0)
+
 if not os.path.exists(BUILD_DIR):
   os.mkdir(BUILD_DIR)
 
 ORIGINAL_DIR = os.getcwd()
 os.chdir(BUILD_DIR)
 
-# configure
+# configure && make && install...
 call(["../zeromq-4.0.4/configure", "--prefix=%s" % BUILD_DIR])
 call(["make", "-j4"])
 call(["make", "install"])
+
+# Copy the C++ bindings over.
+shutil.copyfile("../zeromq-4.0.4/include/zmq.hpp", "include/zmq.hpp")
 
 os.chdir(ORIGINAL_DIR)
